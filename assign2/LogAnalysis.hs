fcefbcb491e90d1f70091c6c86ada06fc0e0d68f
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-05 22:24:32 -0700

    FUCKING FINALLY

--- assign2/LogAnalysis.hs
+++ assign2/LogAnalysis.hs
@@ -2,21 +2,37 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
-import Data.Char
 
 
 -- TODO: Implement
 -- parseMessage :: String -> LogMessage
 
 parseMessageType :: String
-    -> Maybe MessageType
-parseMessageType [] = Nothing
-parseMessageType [_] = Nothing -- This shouldn't be the case!!!!
+    -> (Maybe MessageType, [String])
+parseMessageType [] = (Nothing, [""])
 parseMessageType message =
-  let (typeof:num:_) = words message in
-  let n = read num in
+  let (typeof:msg) = words message in
   case typeof of
-    "I" -> Just Info
-    "W" -> Just Warning
-    "E" -> Just (Error n)
---     _  -> Nothing
+    "I" -> (Just Info, msg)
+    "W" -> (Just Warning, msg)
+    "E" -> (Just (Error (read $ head msg)), drop 1 msg)
+    _  -> (Nothing, msg)
+
+parseTimeStamp :: [String]
+  -> (TimeStamp, [String])
+parseTimeStamp [] = (0 :: TimeStamp, [])
+parseTimeStamp ss = (read $ head ss, drop 1 ss)
+
+wrapit :: [String] -> String
+wrapit [] = ""
+wrapit [x] = x
+wrapit (x:xs) = x ++ " " ++ (wrapit xs)
+
+parseMessage :: String -> LogMessage
+parseMessage [] = Unknown ""
+parseMessage message = 
+  let (typeof, _msg) = parseMessageType message in
+  let (timestamp, msg) = parseTimeStamp _msg in
+  case typeof of
+    Nothing -> Unknown message
+    Just typeof -> LogMessage typeof timestamp (wrapit msg)
