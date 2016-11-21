e75f1e06bea8e81ed86a26da5d832d3bf9a7df39
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-20 22:38:47 -0800

    wft this function

--- assign3/Golf.hs
+++ assign3/Golf.hs
@@ -22,3 +22,26 @@ localMaxima = map fst . filter (\(x,y) -> x > y) . pairwise
 
 pairwise :: [a] -> [(a,a)]
 pairwise as = zip as (tail as)
+
+-- Exercise 3 - Histogram
+zeroToNine = [0 .. 9]
+
+histogram :: [Integer] -> String
+histogram l =
+  -- occurances l ++ "\n"
+  map (\_ -> '=') [0..9] ++ "\n"
+  ++ ['0'..'9'] ++ "\n"
+  where frequency l = map (\x -> count x l) zeroToNine
+        toString = map (\x -> if x > 0 then '*' else ' ')
+        done list = [0] == nub list
+
+decrement = map (\x -> if x > 0 then x - 1 else 0)
+-- onlyValid = filter $ flip elem zeroToNine
+
+count :: Eq a => a -> [a] -> Integer
+count x = genericLength . filter (x==)
+
+-- take all of the valid numbers
+-- map them over zeroToNine
+-- count the number of times it shows in zeroToNine
+countInZeroToNine l = map (\x -> length x) $ group $ sort $ filter (\x -> elem x zeroToNine) l
