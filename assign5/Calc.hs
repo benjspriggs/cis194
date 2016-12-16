5cfea9e4a6f92f02d3c9d7d278d720d85284cc8b
Benjamin Spriggs <benjspriggs@sprico.com>
2016-12-15 17:38:56 -0800

    add imports and such

---
+++ assign5/Calc.hs
@@ -0,0 +1,9 @@
+module Calc where
+import ExprT
+
+-- Exercise 1 - Write Version 1 of the Calculator
+-- eval :: ExprT -> Integer
+-- e.g.:
+-- (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
+eval :: ExprT -> Integer
+eval = undefined
