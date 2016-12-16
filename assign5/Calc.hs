4ac491bbc6daefb7967a4a1f04564a744ce1e408
Benjamin Spriggs <benjspriggs@sprico.com>
2016-12-15 22:02:29 -0800

    qualify references

--- assign5/Calc.hs
+++ assign5/Calc.hs
@@ -1,38 +1,39 @@
+{-# LANGUAGE TypeSynonymInstances #-} -- for Exercise 5
 module Calc where
 import ExprT
 import Parser (parseExp)
-{-# LANGUAGE TypeSynonymInstances #-} -- for Exercise 5
+import StackVM
 
 -- Exercise 1 - Write Version 1 of the Calculator
 -- eval :: ExprT -> Integer
 -- e.g.:
--- (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
+-- (ExprT.Mul (ExprT.Add (Lit 2) (Lit 3)) (Lit 4)) == 20
 eval :: ExprT -> Integer
 eval e = case e of
   Lit n -> n
-  Add l r -> eval l + eval r
-  Mul l r -> eval l * eval r
+  ExprT.Add l r -> eval l + eval r
+  ExprT.Mul l r -> eval l * eval r
 
 -- Exercise 2 - Implement evalString
 -- Using the parser from Parser.hs
 -- evalString :: String -> Maybe Integer
 evalString :: String -> Maybe Integer
-evalString s = case parseExp Lit Add Mul s of
+evalString s = case parseExp Lit ExprT.Add ExprT.Mul s of
   Just e -> Just (eval e)
   Nothing -> Nothing
 
 -- Exercise 3 - Create Expr typeclass
 -- Create a type class such that:
 -- mul (add (lit 2) (lit 3)) (lit 4) :: ExprT
--- == Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+-- == ExprT.Mul (ExprT.Add (Lit 2) (Lit 3)) (Lit 4)
 class (Show a) => Expr a where
   lit :: Integer -> a
   add, mul :: a -> a -> a
 
 instance Expr ExprT where
   lit = Lit
-  add = Add
-  mul = Mul
+  add = ExprT.Add
+  mul = ExprT.Mul
 
 -- Exercise 4 - Extending Functionality
 -- Create instances of Expr for:
@@ -79,5 +80,8 @@ testMM      = testExp :: Maybe MinMax
 testSat     = testExp :: Maybe Mod7
 
 -- Exercise 5 
--- Implement a stack such that
+-- Implement a compiler for a Stack VM
+-- It is true for any exp :: Exp a => a:
 -- stackVM exp == Right [IVal exp]
+compile :: String -> Maybe Program
+compile = undefined
